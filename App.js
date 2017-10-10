3b139efd3c75f936067025e12d43c96e641156c0
coreyarch1234 <harrilal@mit.edu>
2017-10-10 11:43:52 -0700

    started easy books

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Easy Books</Text>
+        <Text>Type in the ISBN of your book.</Text>
+        <Text>Type in your email and the PDF will be sent to you!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ searchBook.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+//Search bar for ISBN.
+
+export default class SearchISBN extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            text: ''
+        }
+
+        render() {
+            return (
+                <View>
+                <TextInput
+                  style={{height: 40}}
+                  placeholder="Type in the ISBN"
+                  onChangeText={(text) => this.setState({text})}
+                  value={this.state.text}
+                />
+                <TouchableOpacity>
+                    style = {styles.submitButton}
+                    onPress = {
+                       () => this.props.onSubmit(this.state.text)
+                    }>
+                    <Text style = {styles.submitButtonText}> Submit </Text>
+                </TouchableOpacity>
+                </View>
+            )
+        }
+    }
+}
+
+
+const styles = {
+
+    submitButton: {
+        backgroundColor: '#7a42f4',
+        width: 135,
+        paddingLeft: 40,
+        paddingTop: 10,
+        borderRadius: 10,
+        height: 40
+     },
+
+    submitButtonText:{
+        color: 'white'
+    }
+}
